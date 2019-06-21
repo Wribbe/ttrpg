5ac4ee032c266779bc4a08993c334a57587c2d37
Stefan Eng <eng_steff@hotmail.com>
2019-06-21 14:32:43 +0200

    Set up template rendering correctly.

--- ttrpg/webapp/__init__.py
+++ ttrpg/webapp/__init__.py
@@ -1,9 +1,14 @@
+import bottle
+
 from bottle import run as bottle_run, template, route
+from pathlib import Path
 
 @route('/')
 def index():
-  return "<h1>Hello World!</h1>"
+  return template("index", name="World")
+
 
 def run(host="localhost", port=8080, debug=False):
   """ Our webapp run method. """
+  bottle.TEMPLATE_PATH.insert(0, Path("ttrpg", "webapp", "views"))
   bottle_run(host=host, port=port, debug=debug, reloader=debug)
---
+++ ttrpg/webapp/views/index.tpl
@@ -0,0 +1,4 @@
+%if name == "World":
+  <h1>Hello {{name}}!</h1>
+  <p>This is a test.</p>
+%end
